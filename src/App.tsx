d94dd5d3b6d15e0ee3ff81b1812fbea7f1b40d3a
Dariusz Franczak <dariusz.franczak@jato.com>
2020-05-12 10:01:49 +0100

    routing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function App() {
   return (
     <div className="App">
-      <>
+      <BrowserRouter>
         <Header />
-        <HomeGuest />
+
+        <Switch>
+          <Route path="/" exact>
+            <HomeGuest />
+          </Route>
+          <Route path="/about-us">
+            <About />
+          </Route>
+          <Route path="/terms">
+            <Terms />
+          </Route>
+        </Switch>
         <Footer />
-      </>
+      </BrowserRouter>
     </div>
   );
 }
